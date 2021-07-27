aaefdeff8856801c44056edc858ecffb40768030
hansinireddyt <85047215+hansinireddyt@users.noreply.github.com>
2021-07-27 16:59:10 +0530

    Add files via upload

---
+++ ComputerArcher.js
@@ -0,0 +1,43 @@
+class ComputerArcher {
+    constructor(x, y, width, height) {
+      var options = {
+        isStatic: true
+      };
+  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/computerArcher.png");
+  
+      World.add(world, this.body);
+      Matter.Body.setAngle (this.body, PI/2)
+    }
+  
+    display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      var angleValue = angle;
+      if (keyIsDown("LEFT") && computerArcher.body.angle<1.77){
+          angleValue=0.1;
+          Matter.Body.setAngle(this.body, angle);
+      }
+      else{
+          angleValue = -0.1
+      }
+
+      if (keyIsDown("RIGHT")&& computerArcher.body.angle>1.47){
+          angleValue = -0.1;
+          Matter.Body.setAngle(this.body, angle);
+      }
+      else{
+          angleValue = 0.1;
+      }
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
